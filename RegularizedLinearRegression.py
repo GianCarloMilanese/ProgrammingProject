bae9303073a6ce66ce98b08343b10cb2d7db8cd2
GianCarloMilanese <giancarlo.milanese@gmail.com>
2018-05-30 19:07:43 +0200

    boh

--- RegularizedLinearRegression.py
+++ RegularizedLinearRegression.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets
+import math
 
 
-class LinearRegression(object):
+class RegularizedLinearRegression(object):
 
     def __init__(self, features=np.array([]), target=np.array([])): #maybe no need for empty array
         self.num_features = len(features[0]) #len of first row
@@ -19,14 +20,18 @@ class LinearRegression(object):
         return np.dot(self.initial_parameters, self.features[example])
 
     "The Cost function J(theta)=1/2m*sum_{i=1^m}(h_theta(x^i)-y^i)^2"
-    def cost(self):
+    def cost(self, lamb):
         my_list = []
         for i in range(self.num_examples):
             my_list.append((self.h_function(i)-self.target[i])**2)
         s = sum(my_list)
-        return (1/(2*self.num_examples))*s
+        parameter_list = []
+        for i in range(1, self.num_features):
+            parameter_list.append(self.initial_parameters[i]**2)
+        ss = sum(parameter_list)
+        return (1/(2*self.num_examples))*(s + lamb*ss)
 
-    def cost_der(self, parameter_index):
+    def cost_der(self, parameter_index, lamb):
         #not sure if parameter_index needs to be in there?
         """
         returns the derivative of the cost function wrt the parameter with index parameter_index
@@ -36,45 +41,45 @@ class LinearRegression(object):
         my_list = []
         for i in range(self.num_examples):
             my_list.append((self.h_function(i)-self.target[i])*self.features[i][parameter_index])
-        return sum(my_list)/self.num_examples
+        return (sum(my_list))/self.num_examples + (lamb*self.initial_parameters[parameter_index])/self.num_examples
 
-    def update_parameters(self, learning_rate=0.01):
+    def update_parameters(self, learning_rate=0.01, lamb=1000):
         new_parameter_list = [] #simultaneous update of parameters
         for i in range(len(self.initial_parameters)):
             new_parameter_list.append(self.initial_parameters[i] -
-                                      learning_rate*self.cost_der(i))
+                                      learning_rate*self.cost_der(i, lamb))
         self.initial_parameters = np.array(new_parameter_list)
 
-    def gradient_descent(self, repetitions, learning_rate):
+    def gradient_descent(self, repetitions, learning_rate, lamb):
         cost_list = []
         for i in range(repetitions):
-            c = self.cost()
-            cost_list.append(self.cost())
+            c = self.cost(lamb)
+            cost_list.append(self.cost(lamb))
             self.update_parameters(learning_rate)
             if i%(repetitions/5) == 0:
                 plt.plot([feature[1] for feature in self.features], self.target, color='g', linewidth=0, marker='o')
                 xs = np.linspace(0, max(np.hstack(self.features)), 100)
                 theta0 = self.initial_parameters[0]
                 theta1 = self.initial_parameters[1]
-                ys = [theta0 + theta1*x for x in xs] #this is the h function
+                ys = [theta0 + theta1*x for x in xs]  # this is the h function
                 plt.plot(xs, ys, color='r')
                 plt.show()
-            if c - self.cost() < 0.0001:
+            if c - self.cost(lamb) < 0.0001:
                 print('convergence at iteration', i)
-        cost_array=np.array(cost_list)
-        plt.plot(range(repetitions),cost_array)
+        cost_array = np.array(cost_list)
+        plt.plot(range(repetitions), cost_array)
         plt.show()
 
-    def gradient_descent2(self, learning_rate):
+    def gradient_descent2(self, learning_rate, lamb):
         repetitions = 0
         cost_list = []
         while True:
-            cost_before = self.cost()
-            cost_list.append(self.cost())
-            self.update_parameters(learning_rate)
+            cost_before = self.cost(lamb)
+            cost_list.append(self.cost(lamb))
+            self.update_parameters(learning_rate, lamb)
 
             if repetitions%500 == 0:   # this plots the hypothesis function; works for the basic case with one feature
-                for i in range(13, len(self.features[0])):
+                for i in range(1, len(self.features[0])):
                     plt.plot([feature[i] for feature in self.features], self.target, color='g', linewidth=0, marker='o')
                     theta0 = self.initial_parameters[0]
                     theta1 = self.initial_parameters[i]
@@ -84,9 +89,9 @@ class LinearRegression(object):
                     plt.title('Iteration {}, feature number {}'.format(repetitions, i))
                     plt.show()
             repetitions += 1
-            #print('cost:', self.cost())
-            if cost_before - self.cost() < 0.0001: # tests convergence
-                for i in range(3, len(self.features[0])):
+            #print('cost:', self.cost(lamb))
+            if np.abs(cost_before - self.cost(lamb)) < 0.0001:  # tests convergence
+                for i in range(1, len(self.features[0])):
                     plt.plot([feature[i] for feature in self.features], self.target, color='g', linewidth=0, marker='o')
                     theta0 = self.initial_parameters[0]
                     theta1 = self.initial_parameters[i]
@@ -125,52 +130,16 @@ class LinearRegression(object):
         return 1 - enum/denom
 
 
-
-
-
-
-
-
 boston = datasets.load_boston()
 features = boston.data
-features_subset = np.vstack(features[:, 5])
+features_subset = np.vstack(features[:, 0])
 target = boston.target
 
-a = LinearRegression(features_subset, target)
+important_features = np.delete(boston.data, [2, 6], 1)
 
-matrix = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
-t = np.array([20, 18, 22, 15, 13, 17, 10, 12, 9, 4, 3])
+c = RegularizedLinearRegression(features, target)  # full boston dataset
 
-a = LinearRegression(features_subset, target)  # Only CRIM feature
-b = LinearRegression(matrix, t)  # small example with one variable
-c = LinearRegression(features, target)  # full boston dataset
-
-
-#b.gradient_descent2(0.03)
-#b.scaling()
-#b.gradient_descent2(0.1)
-
-
-#print(c.features[:,1])
-
-features_without_chas = np.delete(boston.data, 3, 1)  # deletes column with index 3
-
-important_features = np.delete(boston.data, [0,1,2,3,4,6,7,8,9,11], 1)
-
-print(a.features)
-print(min(a.features[:,1]))
-print(max(a.features[:,1]))
-#a.scaling()
-print(min(a.features[:,1]))
-print(max(a.features[:,1]))
-print(a.features)
-
-d = LinearRegression(features_without_chas, target)
-e = LinearRegression(important_features, target)
-
-
-a.scaling()
-a.gradient_descent2(0.5)
-print(a.r2())
-
-
+c.scaling()
+c.gradient_descent2(0.5, 0)
+print(c.r2())
+print(c.initial_parameters[0], np.mean(c.target))
